d647bd85586c5380071b3b74dc3170430786a679
koktoh <koktoh96@gmail.com>
2020-01-17 21:40:57 +0900

    create app

---
+++ hibiki_angel_language_wpf/Models/Config/ConfigFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using BFCore.Config;
+
+namespace BFWpf.Models.Config
+{
+    public class ConfigFactory
+    {
+        public T Create<T>()
+        {
+            var name = typeof(T).Name;
+
+            if (!(typeof(T) == typeof(CommonConfig) || typeof(T) == typeof(BFCommandConfig)))
+            {
+                throw new Exception($"{name} is not approved type.");
+            }
+
+            var di = new DirectoryInfo(Path.Combine(Environment.CurrentDirectory));
+
+            var file = di.GetFiles($"{name}.json", SearchOption.AllDirectories).FirstOrDefault();
+
+            if (file != null && file.Exists)
+            {
+                return ConfigManager.Import<T>(file.OpenRead());
+            }
+            else
+            {
+                return (T)typeof(T).GetConstructor(Type.EmptyTypes).Invoke(null);
+            }
+        }
+    }
+}
---
+++ hibiki_angel_language_wpf/ViewModels/MainViewModel.cs
@@ -0,0 +1,47 @@
+﻿using BFCore.Config;
+using BFWpf.Models.Config;
+using BFWpf.Models.Execute;
+using Prism.Commands;
+using Prism.Mvvm;
+
+namespace BFWpf.ViewModels
+{
+    public class MainViewModel : BindableBase
+    {
+        private string _codeText;
+        private string _output;
+
+        private readonly Executer _executer;
+
+        public string CodeText
+        {
+            get { return this._codeText; }
+            set { SetProperty(ref this._codeText, value); }
+        }
+
+        public string Output
+        {
+            get { return this._output; }
+            set { SetProperty(ref this._output, value); }
+        }
+
+        public DelegateCommand RunCommand { get; private set; }
+
+        public MainViewModel()
+        {
+            this.RunCommand = new DelegateCommand(Execute);
+
+            var factory = new ConfigFactory();
+            this._executer = new Executer(factory.Create<CommonConfig>(), factory.Create<BFCommandConfig>());
+        }
+
+        public void Execute()
+        {
+            this._executer.Reset();
+
+            this._executer.Execute(this.CodeText);
+
+            this.Output = this._executer.Output;
+        }
+    }
+}
